12e40955001e0f308a64ce4c5ddc0c7380794c7b
ChoKyuWon <kyuwoncho18@gmail.com>
2023-09-16 03:46:33 +0900

    symbol:Fix `load_dynsymtab` to support no-plt
    `load_dynsymtab` only loaded the dynamic symbols from `dynsym` and
    PLTs(`.rel(a).plt` and `.plt`). this commit fixes it to call the
    `arch_load_dynsymtab_noplt` in the `load_dynsymtab`, so it also loads the
    dynamic symbols from the `rel(a).dyn`, which normally saves the symbols
    in case of no-plt option.
    
    Signed-off-by: ChoKyuWon <kyuwoncho18@gmail.com>

--- arch/x86_64/symbol.c
+++ arch/x86_64/symbol.c
@@ -29,12 +29,11 @@ int arch_load_dynsymtab_noplt(struct uftrace_symtab *dsymtab, struct uftrace_elf
 
 	memset(dsymtab, 0, sizeof(*dsymtab));
 
-	/* assumes there's only one RELA section (rela.dyn) for no-plt binary */
 	elf_for_each_shdr(elf, &sec_iter) {
-		if (sec_iter.shdr.sh_type == SHT_RELA) {
+		if (strcmp(elf_get_name(elf, &sec_iter, sec_iter.shdr.sh_name), ".rela.dyn") == 0) {
 			memcpy(&rel_iter, &sec_iter, sizeof(sec_iter));
-			pr_dbg2("found RELA section: %s\n",
-				elf_get_name(elf, &sec_iter, sec_iter.shdr.sh_name));
+			pr_dbg2("found rela.dyn section with %ld entry.\n",
+				sec_iter.shdr.sh_entsize);
 
 			reloc_start = rel_iter.shdr.sh_addr + offset;
 			reloc_entsize = rel_iter.shdr.sh_entsize;
@@ -53,6 +52,8 @@ int arch_load_dynsymtab_noplt(struct uftrace_symtab *dsymtab, struct uftrace_elf
 		struct uftrace_symbol *sym;
 		int symidx;
 		char *name;
+		size_t k;
+		bool found = false;
 
 		symidx = elf_rel_symbol(&rel_iter.rela);
 		if (symidx == 0)
@@ -76,6 +77,15 @@ int arch_load_dynsymtab_noplt(struct uftrace_symtab *dsymtab, struct uftrace_elf
 			dsymtab->nr_alloc += grow;
 			dsymtab->sym = xrealloc(dsymtab->sym, dsymtab->nr_alloc * sizeof(*sym));
 		}
+		name = elf_get_name(elf, &sym_iter, sym_iter.sym.st_name);
+		for (k = 0; k <= noplt_skip_nr; k++) {
+			if (strcmp(name, noplt_skip_syms[k]) == 0) {
+				found = true;
+				break;
+			}
+		}
+		if (found)
+			continue;
 
 		sym = &dsymtab->sym[dsymtab->nr_sym++];
 
@@ -84,7 +94,6 @@ int arch_load_dynsymtab_noplt(struct uftrace_symtab *dsymtab, struct uftrace_elf
 		sym->size = reloc_entsize;
 		sym->type = ST_PLT_FUNC;
 
-		name = elf_get_name(elf, &sym_iter, sym_iter.sym.st_name);
 		if (flags & SYMTAB_FL_DEMANGLE)
 			sym->name = demangle(name);
 		else
@@ -93,6 +102,7 @@ int arch_load_dynsymtab_noplt(struct uftrace_symtab *dsymtab, struct uftrace_elf
 		pr_dbg3("[%zd] %c %lx + %-5u %s\n", dsymtab->nr_sym, sym->type, sym->addr,
 			sym->size, sym->name);
 	}
+	sort_dynsymtab(dsymtab);
 
 	return dsymtab->nr_sym;
 }
